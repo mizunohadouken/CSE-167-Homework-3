c72acb5819abb63743f09b9d8ffefc6bfac61743
DESKTOP-JN1FQR8\Andrew <andrewmvalenz@gmail.com>
2017-11-17 22:38:58 -0800

    Output basic .png gradient using FreeImage

--- hw3 code/Ray Test/main.cpp
+++ hw3 code/Ray Test/main.cpp
@@ -6,6 +6,29 @@ int main(int argc, char *argv[])
 {
 	
 	FreeImage_Initialise();
+
+	int Width = 500, Height = 500, BitsPerPixel = 24; // TODO change variables based on parser
+
+
+	FIBITMAP *bitmap = FreeImage_Allocate(Width, Height, BitsPerPixel);
+	RGBQUAD color;
+
+	for (int i = 0; i < Width; i++)
+	{
+		for (int j = 0; j < Height; j++)
+		{
+			color.rgbRed = (double)i / Width * 255.0;
+			color.rgbGreen = 0;
+			color.rgbGreen = (double)j / Height * 255.0;
+			FreeImage_SetPixelColor(bitmap, i, j, &color);
+		}
+	}
+
+	FreeImage_Save(FIF_PNG, bitmap, "filename.png", 0);
+
+
+
+	// Display Test
 	std::cout << "Free Image_" << FreeImage_GetVersion() << "\n";
 	std::cout << FreeImage_GetCopyrightMessage() << "\n\n";
 	FreeImage_DeInitialise();
